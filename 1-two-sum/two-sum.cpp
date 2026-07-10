18e87e750a09a19fbb52a4383025b926f1111c16
Addison Chang <301668848+AddisonChangCS@users.noreply.github.com>
2026-07-10 22:18:53 +0800

    Time: 3 ms (66.81%) | Memory: 14.9 MB (19.21%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
+    //Time: O(N)
+    //Space: O(N)
     vector<int> twoSum(vector<int>& nums, int target) {
-        //Time: O(N)
-        //Space: O(N)
         unordered_map<int,int> seen;
         for (int i = 0; i < nums.size(); i++)
         {
