3ad6d4962cbdab23ed707884fc91cb2adff1e650
Addison Chang <301668848+AddisonChangCS@users.noreply.github.com>
2026-07-10 22:17:54 +0800

    Time: 4 ms (32.05%) | Memory: 8.5 MB (31.83%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 class Solution {
 public:
+    //Time: O(M+N) 
+    //Space: O(1)
     string mergeAlternately(string word1, string word2) {
-        //Time: O(M+N)
-        //Space: O(1)
         int length1=word1.size(), length2=word2.size();
         string result="";
         for (int i = 0; i < max(length1,length2); i++)
