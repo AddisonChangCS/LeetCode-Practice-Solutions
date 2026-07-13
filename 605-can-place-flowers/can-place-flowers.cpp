08ae530bb8341275a32d604b2576d1625a51a3cb
Addison Chang <301668848+AddisonChangCS@users.noreply.github.com>
2026-07-13 10:03:48 +0800

    Time: 0 ms (100.00%) | Memory: 24.2 MB (44.87%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    //Time: O(N)
+    //Space: O(1)
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) 
+    {
+        for (int i = 0; i < flowerbed.size(); i++)
+        {
+           if (flowerbed[i]==0)
+           {
+                bool left_empty=(i==0 || flowerbed[i-1]==0);
+                bool right_empty=(i==flowerbed.size()-1 || flowerbed[i+1]==0);
+
+                if (left_empty && right_empty)
+                {
+                    flowerbed[i]=1;
+                    n--;
+                }
+           }
+           if (n<=0)
+           {
+                return true;
+           }
+        }
+
+        return false;
+    }
+};
