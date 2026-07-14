6b3b9f3e686afa4a98067c593c1da50443b5214d
Addison Chang <301668848+AddisonChangCS@users.noreply.github.com>
2026-07-14 18:02:24 +0800

    Time: 79 ms (91.94%) | Memory: 61.8 MB (98.53%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,21 +1,23 @@
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        return solution1(nums, k);
+        return solution2(nums, k);
     }
 private:
+
     //using hash tabel
     //Time: O(N)
     //Space: O(N)
     int solution1(vector<int>& nums, int k){
         
+        int operation_count=0;
+        
         unordered_map<int,int> count;
         count.reserve(nums.size());
-
-        int operation_count=0;
 
         for (int i = 0; i < nums.size(); i++)
         {
@@ -29,6 +31,36 @@ private:
             else
             {
                 count[nums[i]]++;
+            }
+        }
+        return operation_count;
+    }
+
+    //using two pointer
+    //Time: O(NlogN)
+    //Space: O(1)
+    int solution2(vector<int>& nums, int k){
+        
+        int operation_count=0;
+
+        sort(nums.begin(),nums.end());
+
+        for (int left_ptr=0, right_ptr=nums.size()-1; left_ptr < right_ptr;)
+        {
+            int sum=nums[left_ptr]+nums[right_ptr];
+            if (sum<k)
+            {
+                left_ptr++;
+            }
+            else if (sum>k)
+            {
+                right_ptr--;
+            }
+            else
+            {
+                operation_count++;
+                left_ptr++;
+                right_ptr--;
             }
         }
         return operation_count;
