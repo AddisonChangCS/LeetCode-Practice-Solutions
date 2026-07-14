716d79aa7e83c16ad02a0acb9e1e8404969a3af4
Addison Chang <301668848+AddisonChangCS@users.noreply.github.com>
2026-07-14 17:12:54 +0800

    Time: 0 ms (100.00%) | Memory: 62.9 MB (49.15%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -8,17 +8,15 @@ public:
         int max_area=0;
         for (int left_ptr = 0, right_ptr = height.size()-1; left_ptr < right_ptr;)
         {
-            int cur_area=0;
+            int cur_area=min(height[right_ptr],height[left_ptr]) * (right_ptr-left_ptr);
+            max_area=max(max_area,cur_area);
+            
             if (height[left_ptr]>height[right_ptr])
             {
-                cur_area=height[right_ptr]*(right_ptr-left_ptr);
-                max_area = (max_area<cur_area) ? cur_area : max_area;
                 right_ptr--;
             }
-            else if (height[left_ptr]<=height[right_ptr])
+            else
             {
-                cur_area=height[left_ptr]*(right_ptr-left_ptr);
-                max_area = (max_area<cur_area) ? cur_area : max_area;
                 left_ptr++;
             }
         }
